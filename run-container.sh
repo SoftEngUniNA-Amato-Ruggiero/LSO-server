375d342ea1cb3ae49857ceb17c9a2f1c3dcdc972
gyus-e <6dfdac64-b799-4068-9972-4a1ea68da044@blackfellows.anonaddy.me>
2025-02-26 17:17:41 +0100

    fix run-container.sh

--- run-container.sh
+++ run-container.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-if [ $1 == "podman" ]; then
+if [ "$1" == "podman" ]; then
     docker="podman"
 else
     docker="docker"
 fi
 
 $docker build -t lso-server .
-$docker run -it -p 9999:9999 --name running-lso-server lso-server
+$docker run -it --rm -p 9999:9999 --name running-lso-server lso-server
