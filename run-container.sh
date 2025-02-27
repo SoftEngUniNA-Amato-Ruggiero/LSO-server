0c83d644bac6e4d6f49d62a9cc9723e0ed1a45d1
gyus-e <6dfdac64-b799-4068-9972-4a1ea68da044@blackfellows.anonaddy.me>
2025-02-27 03:01:56 +0100

    added podman remote support

--- run-container.sh
+++ run-container.sh
@@ -2,6 +2,8 @@
 
 if [ "$1" == "podman" ]; then
     docker="podman"
+elif [ "$1" == "podman-remote" ]; then # for podman on MacOS or WSL
+    docker="podman-remote-static-linux_amd64"
 else
     docker="docker"
 fi
